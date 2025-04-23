e65641b531fff71f6fa70399915a609b8dbee4c8
curlyheadcoder <mayanksonwani078@gmail.com>
2025-04-24 02:08:19 +0530

    Time: 19 ms (9.36%), Space: 41.4 MB (36.55%) - LeetHub

--- 1500-count-largest-group/1500-count-largest-group.java
+++ 1500-count-largest-group/1500-count-largest-group.java
@@ -1,26 +1,28 @@
 class Solution {
+    private int digitSum(int n){
+        int sum = 0;
+        while(n > 0){
+            sum += (n%10);
+            n /= 10;
+        }
+        return sum;
+    }
     public int countLargestGroup(int n) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap <Integer, Integer> mapSize = new HashMap<>();
         int maxSize = 0;
+        // Calculate digit sum for each num and update map size
         for(int i = 1; i <= n; i++){
-            int sum = getDigitSum(i);
-                map.put(sum, map.getOrDefault(sum, 0) + 1);
-                maxSize = Math.max(maxSize, map.get(sum));
-            }
-            int count = 0;
-            for(int size : map.values()){
-                if(size == maxSize){
-                    count++;
-                }
-            }
-            return count;
+            int sum = digitSum(i);
+            mapSize.put(sum, mapSize.getOrDefault(sum, 0)+1);
+            maxSize = Math.max(maxSize, mapSize.get(sum));
         }
-        private int getDigitSum(int num){
-            int sum = 0;
-            while(num > 0){
-                sum = sum + num%10;
-                num = num/10;
+        // Count the num of groups with the maximum size
+        int count = 0;
+        for(int map : mapSize.values()){
+            if(map == maxSize){
+                count++;
             }
-            return sum;
         }
+        return count;
+    }
 }
