e186f5537c45b4024a55f2125a48eb657f4517ec
curlyheadcoder <mayanksonwani078@gmail.com>
2025-05-22 01:16:02 +0530

    Time: 1 ms (72.87%), Space: 45.7 MB (66.65%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,35 +1,22 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] temp = new int[m][n];
-
-        // Copy original matrix to temp
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                temp[i][j] = matrix[i][j];
-            }
-        }
-
-        // Set zeroes in temp based on zeros in original matrix
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    for (int k = 0; k < n; k++) {
-                        temp[i][k] = 0;  // set i-th row to 0
-                    }
-                    for (int k = 0; k < m; k++) {
-                        temp[k][j] = 0;  // set j-th column to 0
-                    }
+        int m = matrix.length, n = matrix[0].length;
+        boolean [] row = new boolean [m];
+        boolean [] col = new boolean [n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
-
-        // Copy temp back to matrix
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = temp[i][j];
+        for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    if(row[i] || col[j]){
+                        matrix[i][j] = 0;
+                    }
+                }
             }
-        }
     }
 }
