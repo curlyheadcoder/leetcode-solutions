a7bae862e3b2ea75629fd353c0fac97462bc4482
curlyheadcoder <mayanksonwani078@gmail.com>
2025-06-07 01:21:55 +0530

    Time: 28 ms (99.37%), Space: 45.8 MB (97.47%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.java
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.java
@@ -2,25 +2,23 @@ class Solution {
     public String robotWithString(String s) {
         int [] freq = new int [26];
         for(char ch : s.toCharArray()){
-            freq[ch-'a']++;
+            freq[ch - 'a']++;
         }
-        Stack<Character> stack = new Stack<>();
+        char [] stack = new char [s.length()];
+        int top = -1;
         StringBuilder res = new StringBuilder();
         int smallest = 0;
         for(char ch : s.toCharArray()){
-            stack.push(ch);
-            freq[ch - 'a']--;
-
+            freq[ch -'a']--;
+            stack[++top] = ch;
             while(smallest < 26 && freq[smallest] == 0){
                 smallest++;
             }
-            while(!stack.isEmpty() && stack.peek() - 'a' <= smallest){
-                res.append(stack.pop());
+            while(top >= 0 && (stack[top] - 'a') <= smallest){
+                res.append(stack[top--]);
             }
         }
-        while(!stack.isEmpty()){
-            res.append(stack.pop());
-        }
+        while(top >= 0) res.append(stack[top--]);
         return res.toString();
     }
 }
