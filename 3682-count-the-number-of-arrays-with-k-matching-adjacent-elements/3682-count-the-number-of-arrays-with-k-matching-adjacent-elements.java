00f520ee9c9e80b69a9fe3ff45aafb9a2f0d0afa
curlyheadcoder <mayanksonwani078@gmail.com>
2025-06-17 15:14:50 +0530

    Time: 139 ms (14.63%), Space: 45.4 MB (29.27%) - LeetHub

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.java
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.java
@@ -1,46 +1,43 @@
 class Solution {
-    int MOD = 1000000007;
+    static final int MOD = 1000000007;
+    int [] fact;
+    int [] invFact;
     public int countGoodArrays(int n, int m, int k) {
-        	int adjPairPossible = n - 1; // out of these i have to find k pair such that adjacent elements are equal
-		//System.out.println(combination);
-		int combination =(int)((1L* m*find(adjPairPossible, k))%MOD);  // for putting elements on adj to each other
-		System.out.println(combination);
-
-		int secCombination = BinaryExpo(m-1,adjPairPossible -k); // Combination for adjacent pair which are not equal
-	System.out.println(secCombination);
-		return (int) ((1L*combination*secCombination)%MOD);
+        int adjPairPossible = n - 1;
+        // Precompute factorials upto n
+        preComputeFactorials(adjPairPossible);
+        // C(n-1,k)*m
+        int combination = (int)((1L * m * nCk(adjPairPossible, k)) % MOD);
+        // (m-1)^(n-1-k)
+        int secCombination = binaryExpo(m-1, adjPairPossible - k);
+        return (int)((1L * combination * secCombination) % MOD);
     }
-    private  int BinaryExpo(int n, int pow) {
-		if(pow ==0) {
-			return 1; 
-		}
-		int val = BinaryExpo(n,pow/2);
-		long res= (int)((1L*val*val)%MOD);
-		if(pow%2==0) {
-			return (int)res;
-		}
-		else {
-			return (int)((res*1L*n)%MOD);
-		}
-	}
-
-	private  int find(int adjPairPossible, int k) {
-		// TODO Auto-generated method stub
-		int n = fact(adjPairPossible);
-		
-		int lb = (int)((1L*fact(k)*fact(adjPairPossible-k))%MOD);
-		
-		return  (int)((1L*n*modInverse(lb))%MOD);
-	}
-
-	private  int modInverse(int lb) {  // Little fermat theorem
-		// TODO Auto-generated method stub
-		return BinaryExpo(lb,MOD-2);
-		
-	}
-
-	private  int fact(int n) {
-		   if(n==0||n==1) return 1;
-           return (int)((1L*n*fact(n-1))%MOD);
-	}
+    void preComputeFactorials(int n){
+        fact = new int [n+1];
+        invFact = new int [n+1];
+        fact[0] = 1;
+        for(int i = 1; i <= n; i++){
+            fact[i] = (int)((1L * fact[i-1] * i) % MOD);
+        }
+        invFact[n] = binaryExpo(fact[n], MOD-2);
+        for(int i = n - 1; i >= 0; i--){
+            invFact[i] = (int)((1L * invFact[i+1] * (i+1)) % MOD);
+        }
+    }
+    int nCk(int n, int k){
+        if(k < 0 || k > n) return 0;
+        return (int)((1L * fact[n] * invFact[k] % MOD) * invFact[n-k] % MOD);
+        }
+    int binaryExpo(int base, int exp){
+        int res = 1;
+        long b = base;
+        while(exp > 0){
+            if((exp & 1) == 1){
+                res = (int)((1L * res * b) % MOD);
+            }
+            b = (b * b) % MOD;
+            exp >>= 1;
+        }
+        return res;
+    }
 }
