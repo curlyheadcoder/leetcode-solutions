32a38cd89e497973271f39b74d2e9f449bacbd6e
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-07-04 02:49:00 +0530

    Time: 44 ms (7.58%), Space: 46.4 MB (21.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        for(int j = 0; j < nums.length; j++){
-            if(nums[j] != 0){
-                nums[i] = nums[j];
-                i++; 
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                for(int j = i + 1; j < nums.length; j++){
+                    if(nums[j] != 0){
+                        swap(nums, i, j);
+                        break;
+                    }
+                }
             }
         }
-        while(i < nums.length){
-            nums[i] = 0;
-            i++;
-        }
+    }
+    private void swap(int [] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
