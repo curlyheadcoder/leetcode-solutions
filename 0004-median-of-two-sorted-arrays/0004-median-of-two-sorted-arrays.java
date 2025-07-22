bb8aab3855b508ab86c28b1d1d58c5125a069f88
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-07-22 16:46:17 +0530

    Time: 1 ms (100%), Space: 46.1 MB (59.94%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,53 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2, nums1);
+        }
         int m = nums1.length;
         int n = nums2.length;
-        int size = m + n;
 
-        int idx1 = (size/2) - 1;
-        int element1 = -1;
+        int l = 0, r = m;
 
-        int idx2 = (size/2);
-        int element2 = -1;
+        while(l <= r){
+            int Px = l + (r-l) / 2;    // mid
+            int Py = (m+ n + 1) / 2 - Px; // from nums2
 
-        int i = 0;  // nums1
-        int j = 0;  // nums2
-
-        int k = 0;
-
-        while(i < m && j < n){
-            if(nums1[i] < nums2[j]){
-
-                if(k == idx1) element1 = nums1[i];
-                if(k == idx2) element2 = nums1[i];
-                i++;
-            }else{
-                
-                if(k == idx1) element1 = nums2[j];
-                if(k == idx2) element2 = nums2[j];
-                j++;
-            }
-            k++;
-        }
-
-        while(i < m){
+            // Left half waale
+            int x1 = (Px == 0) ? Integer.MIN_VALUE : nums1[Px - 1];
+            int x2 = (Py == 0) ? Integer.MIN_VALUE  : nums2[Py - 1];
             
-            if(k == idx1) element1 = nums1[i];
-            if(k == idx2) element2 = nums1[i];
-            i++;
-            k++;
-        }
-        while(j < n){
+            // Right half wale 
+            int x3 = (Px == m) ? Integer.MAX_VALUE : nums1[Px];
+            int x4 = (Py == n) ? Integer.MAX_VALUE :nums2[Py];
 
-            if(k == idx1) element1 = nums2[j];
-            if(k == idx2) element2 = nums2[j];
-            j++;
-            k++;
-        }
+            if(x1 <= x4 && x2 <= x3){
+                if((m+n) % 2 == 1){
+                    return Math.max(x1, x2);
+                }
+                return (Math.max(x1,x2) + Math.min(x3, x4)) / 2.0;
+            }
 
-        if(size % 2 == 1){    // odd
-            return element2;
+            if(x1 > x4) r = Px - 1;
+            else l = Px + 1;
         }
-        return (element1 + element2)/2.0;
+        return -1;
     }
 }
