48c29f572f4eb3674c1877f3a7530d4e8be7e7f3
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-07-25 14:00:28 +0530

    Time: 9 ms (3.13%), Space: 42.8 MB (28.13%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,14 +1,7 @@
 class Solution {
     public int maxSum(int[] nums) {
-        Set<Integer> posElem = new HashSet<>();
-        for(int num : nums){
-            if(num > 0){
-                posElem.add(num);
-            }
-        }
-        if(posElem.isEmpty()){
-            return Arrays.stream(nums).max().getAsInt();
-        }
-        return posElem.stream().mapToInt(Integer::intValue).sum();
+        int max = Arrays.stream(nums).max().getAsInt();
+        int sum = Arrays.stream(nums).filter(x -> x > 0).distinct().sum();
+        return sum > 0 ? sum : max;
     }
 }
