5926cc760ec903e1dc8d563cb2aa0959a6613135
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-08-01 12:28:49 +0530

    Time: 0 ms (100%), Space: 41.9 MB (91%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,15 +1,19 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        /* Using Combinations (nCr Formula)
+            Each element at index j in row i is C(i, j): C(n, r) = n!/(r! * (n-r)!)
+            We can precompute factorials or use iterative formula to compute 
+            C(i, j) efficiently without recomputing
+        */
         List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0; i < numRows; i++){
+        for(int n = 0; n < numRows; n++){
             List<Integer> row = new ArrayList<>();
-            for(int j = 0; j <= i; j++){
-                if(j == 0 || j == i){
-                    row.add(1);    // First and last element
-                }else{
-                    int sum = res.get(i-1).get(j-1) + res.get(i-1).get(j);
-                    row.add(sum);
-                }
+            int val = 1;     // C(n,0) is always 1
+            for(int k = 0; k <= n; k++){
+                row.add(val);
+                // Compute next value using the property
+                // C(n , k+1) = C(n,k) * (n-k)/(k+1)
+                val = val * (n - k) / (k + 1);
             }
             res.add(row);
         }
