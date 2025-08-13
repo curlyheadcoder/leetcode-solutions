f15152ccc5ef45b323841ee8fd485b82c72b1982
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-08-13 15:52:43 +0530

    Time: 7 ms (100%), Space: 44.5 MB (18.53%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        int maxPower = (int) Math.pow(3,19);
-        return n>0 && (maxPower % n == 0);
+        // int maxPower = (int) Math.pow(3,19);
+        return n>0 && (1162261467 % n == 0);
     }
 }
