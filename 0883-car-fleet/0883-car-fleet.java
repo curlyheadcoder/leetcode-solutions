2b5132da713bbe3da2b4a118464cf0c641142944
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-08-19 22:41:30 +0530

    Time: 78 ms (69.79%), Space: 57.8 MB (46.58%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -1,20 +1,21 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
-        Integer [] indices = new Integer [n];
+        Integer [] indices = new Integer[n];
         for(int i = 0; i < n; i++){
             indices[i] = i;
         }
-        Arrays.sort(indices, (a,b) -> position[b] - position[a]);
-        int fleetCount = 0;
-        double lastTime = 0.0;
+        // Sort the cars by position descending (closest to the target first)
+        Arrays.sort(indices, (a, b) -> position[b] - position[a]);
+
+        Stack<Double> st = new Stack<>();
         for(int i : indices){
-            double time = (double)(target - position[i]) / speed[i];
-            if(time > lastTime){
-                fleetCount++;
-                lastTime = time;
+            double time = (double) (target - position[i]) / speed[i];
+            if(st.size() == 0 || time > st.peek()){
+                st.push(time);  // new fleet
             }
+            // else current car joins fleet on top of stack
         }
-        return fleetCount;
+        return st.size();
     }
 }
