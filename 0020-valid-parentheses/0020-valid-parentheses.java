660cd2fb07293f39c4fae942f9ea507ef4f79a10
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-08-20 16:32:32 +0530

    Time: 2 ms (96.56%), Space: 41.7 MB (66.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if(n % 2 != 0) return false;
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()) return false;
+                char top = st.pop();
+                if(ch == ')' && top != '('){
+                    return false;
+                }else if(ch == ']' && top != '['){
+                    return false;
+                }else if(ch == '}' && top != '{'){
+                    return false;
+                }
+            }
+        }
+        return st.size() == 0;
+    }
+}
