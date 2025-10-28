4a6f7659f9f886f2f930403f49c399cf17e9cba4
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-10-28 20:20:24 +0530

    Time: 1 ms (83.13%), Space: 42.3 MB (48.19%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
@@ -1,36 +1,27 @@
 class Solution {
-    private boolean isValid(int[] nums, int count, int i, int direction) {
-        int[] temp = nums.clone();
-        int idx = i;
-
-        while (count > 0 && idx >= 0 && idx < temp.length) {
-            if (temp[idx] > 0) {
-                temp[idx]--;
-                direction *= -1;
-                if (temp[idx] == 0) {
-                    count--;
-                }
-            }
-            idx += direction;
-        }
-
-        return count == 0; // all elements become 0
-    }
-
     public int countValidSelections(int[] nums) {
         int n = nums.length;
         int result = 0;
-        int count = 0;
+        int currSum = 0;
+        int S = 0;
 
         for (int num : nums) {
-            if (num != 0) count++;
+            S += num;
         }
 
         for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                if (isValid(nums, count, i, -1)) result++;
-                if (isValid(nums, count, i, 1)) result++;
-            }
+            currSum += nums[i];
+
+            int left  = currSum;
+            int right = S - left;
+
+            if (nums[i] != 0) 
+                continue;
+
+            if (left == right)
+                result += 2;
+            else if (Math.abs(left - right) == 1)
+                result++;
         }
 
         return result;
