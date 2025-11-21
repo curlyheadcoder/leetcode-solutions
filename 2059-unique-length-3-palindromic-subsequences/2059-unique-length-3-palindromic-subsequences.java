e109ec829084bbf68ada4b1bd85dba8f04ccf601
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-11-21 17:24:38 +0530

    Time: 204 ms (48.02%), Space: 46.9 MB (28.57%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int [] fo = new int [26], lo = new int [26];
+        Arrays.fill(fo, Integer.MAX_VALUE);
+        for(int i = 0; i < s.length(); i++){
+            fo[s.charAt(i) - 'a'] = Math.min(fo[s.charAt(i) - 'a'], i);
+            lo[s.charAt(i) - 'a'] = i;
+        }
+        int ans = 0;
+        for(int i = 0; i < 26; i++){
+            int f = fo[i], l = lo[i];
+            Set<Character> unique = new HashSet<>();
+            if(f < l){
+                for(int j = f + 1; j < l; j++){
+                    unique.add(s.charAt(j));
+                }
+            }
+            ans += unique.size();
+        }
+        return ans;
+        }
+    
+}
