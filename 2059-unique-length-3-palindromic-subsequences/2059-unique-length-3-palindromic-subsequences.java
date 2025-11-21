14e29eadba88ee88a4ae58abe54f8a1747fe176d
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-11-21 17:25:00 +0530

    Time: 38 ms (95.44%), Space: 46.9 MB (23.4%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,23 +1,30 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int [] fo = new int [26], lo = new int [26];
-        Arrays.fill(fo, Integer.MAX_VALUE);
-        for(int i = 0; i < s.length(); i++){
-            fo[s.charAt(i) - 'a'] = Math.min(fo[s.charAt(i) - 'a'], i);
-            lo[s.charAt(i) - 'a'] = i;
+        int n = s.length();
+        int [] first = new int [26];
+        int [] last = new int [26];
+        Arrays.fill(first, -1);
+        Arrays.fill(last, -1);
+        for(int i = 0; i < n; ++i){
+            int c = s.charAt(i) - 'a';
+            if(first[c] == -1){
+                first[c] = i;
+            }
+            last[c] = i;
         }
         int ans = 0;
-        for(int i = 0; i < 26; i++){
-            int f = fo[i], l = lo[i];
-            Set<Character> unique = new HashSet<>();
-            if(f < l){
-                for(int j = f + 1; j < l; j++){
-                    unique.add(s.charAt(j));
+        // for each possible end char, collect distinct middle chars between first and last
+        for(int c = 0; c < 26; c++){
+            if(first[c] != -1 && first[c] < last[c]){
+                boolean [] seen = new boolean[26];
+                for(int k = first[c] + 1; k < last[c]; ++k){
+                    seen[s.charAt(k) - 'a'] = true;
+                }
+                for(int m = 0; m < 26; m++){
+                    if(seen[m]) ans++;
                 }
             }
-            ans += unique.size();
         }
         return ans;
-        }
-    
+    }
 }
