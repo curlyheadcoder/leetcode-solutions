76ca7979e137e65dca2a68e40b7850648a20c715
Mayank Sonwani <mayanksonwani078@gmail.com>
2025-12-31 16:42:23 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -5,20 +5,25 @@ class Solution {
 
         while(mid <= right){
             if(nums[mid] == 0){
-                swap(nums, left, mid);
+                int temp = nums[left];
+                nums[left] = nums[mid];
+                nums[mid] = temp;
                 left++;
                 mid++;
             }else if(nums[mid] == 2){
-                swap(nums, right, mid);
+                int temp = nums[mid];
+                nums[mid] = nums[right];
+                nums[right] = temp;
                 right--;
             }else{
                 mid++;
             }
         }
     }
-    private static void swap(int [] nums, int pos1, int pos2){
-        int temp = nums[pos1];
-        nums[pos1] = nums[pos2];
-        nums[pos2] = temp;
-    }
 }
+//     private static void swap(int [] nums, int pos1, int pos2){
+//         int temp = nums[pos1];
+//         nums[pos1] = nums[pos2];
+//         nums[pos2] = temp;
+//     }
+// }
