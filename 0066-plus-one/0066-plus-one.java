1ef6350d3293ceb8b5863f6b8000433f11c08439
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-01-01 16:12:13 +0530

    Time: 0 ms (100%), Space: 43.3 MB (58.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for(int i = digits.length - 1; i >= 0; i--){
-            if(digits[i] < 9){
+        int n = digits.length;
+        for(int i = n-1; i >= 0; i--){
+            if(digits[i] == 9){
+                digits[i] = 0;
+            }else{
                 digits[i]++;
                 return digits;
             }
-            digits[i] = 0;
         }
-        return java.util.stream.IntStream
-            .concat(java.util.stream.IntStream.of(1), java.util.stream.IntStream.generate(() -> 0).limit(digits.length))
-            .toArray();
+        digits = new int [n+1];
+        digits[0] = 1;
+        return digits;
     }
 }
