925b50227bed1cd59df56871de7bf63fa5c8d674
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-01-01 18:32:46 +0530

    Time: 3 ms (76.87%), Space: 52.4 MB (61.98%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,15 +3,14 @@ class Solution {
         int maj1 = 0, maj2 = 0;
         int count1 = 0, count2 = 0;
         int n = nums.length;
-
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < n; i++){
             if(nums[i] == maj1){
                 count1++;
             }else if(nums[i] == maj2){
                 count2++;
             }else if(count1 == 0){
-                maj1 = nums[i] ;
-                count1 = 1;
+                maj1 = nums[i];
+                count1 = 1; 
             }else if(count2 == 0){
                 maj2 = nums[i];
                 count2 = 1;
@@ -20,18 +19,15 @@ class Solution {
                 count2--;
             }
         }
-        // verification
-
         int freq1 = 0, freq2 = 0;
-        List<Integer> res = new ArrayList<>();
         for(int num : nums){
             if(num == maj1) freq1++;
             else if(num == maj2) freq2++;
         }
+        List<Integer> ans = new ArrayList<>();
+        if(freq1 > n/3) ans.add(maj1);
+        if(freq2 > n/3) ans.add(maj2);
 
-        if(freq1 > n/3) res.add(maj1);
-        if(freq2 > n/3) res.add(maj2);
-
-        return res;
+        return ans;
     }
 }
