49b30ff8c4bff77044b2e7e5cc2cc70156f4b29e
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-02-11 15:56:57 +0530

    Time: 25 ms (100%), Space: 47.7 MB (11.4%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -1,23 +1,40 @@
 class Solution {
+    // Arrays ko bahar rakho taaki baar-baar allocate na ho
+    int[] s1 = new int[100001]; 
+    int[] s2 = new int[100001];
     public int longestBalanced(int[] nums) {
         int n = nums.length;
-        int maxLen = 0;
-        for(int i = 0; i < n; i++){
-            Map<Integer, Integer> freq = new HashMap<>();
-            int disEven = 0;
-            int disOdd = 0;
-            for(int j = i; j < n; j++){
-                int num = nums[j];
-                freq.put(num, freq.getOrDefault(num, 0)+1);
-                if(freq.get(num) == 1){   // first time seen
-                    if(num % 2 == 0) disEven++;
-                    else disOdd++;
+        int max = 0;
+        int timer = 0; // Har naye test case ke liye reset (Optional)
+        for (int i = 0; i < n; i++) {
+            // "new" karne ke bajaye bas timer badhao
+            timer++; 
+            int ev = 0;
+            int od = 0;
+
+            // Optimization: Agar bacha hua rasta max se chota h toh skip
+            if (max >= n - i) break; 
+
+            for (int j = i; j < n; j++) {
+                int val = nums[j];
+                if (val % 2 == 0) {
+                    // Agar s1[val] timer ke barabar nahi h, matlab is loop me naya h
+                    if (s1[val] != timer) {
+                        s1[val] = timer;
+                        ev++;
+                    }
+                } else {
+                    if (s2[val] != timer) {
+                        s2[val] = timer;
+                        od++;
+                    }
                 }
-                if(disEven == disOdd){
-                    maxLen = Math.max(maxLen, j - i + 1);
+
+                if (ev == od) {
+                    max = Math.max(max, j - i + 1);
                 }
             }
         }
-        return maxLen;
+        return max;
     }
 }
