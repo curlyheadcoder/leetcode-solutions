d189e5c95ed2585c99c6fe3fefec875ae677ace8
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-02-14 17:17:08 +0530

    Time: 4 ms (32.16%), Space: 48.6 MB (21.2%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double [][] dp = new double [101][101];
+        dp[0][0] = poured;
+        for(int row = 0; row < query_row; row++){
+            for(int col = 0; col <= row; col++){
+                double overflow = Math.max(0, dp[row][col]-1);
+                if(overflow > 0){
+                    dp[row+1][col] += overflow / 2.0;
+                    dp[row+1][col+1] += overflow / 2.0;
+                }
+            }
+        }
+        return Math.min(1, dp[query_row][query_glass]);
+    }
+}
