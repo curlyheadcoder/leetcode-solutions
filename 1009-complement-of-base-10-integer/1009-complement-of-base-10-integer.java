dcd9ba9482269615b9bc5e6b4891c01aa7759bd4
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-03-11 13:37:41 +0530

    Time: 0 ms (100%), Space: 42.1 MB (66.82%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,16 +1,17 @@
 class Solution {
     public int bitwiseComplement(int n) {
+        // Base case:
         if(n == 0) return 1;
-        int res = 0;
-        int counter = 0;
-        while(n > 0){
-            int r = n % 2;
-            if(r == 0){
-                res += (int) Math.pow(2, counter);
-            }
-            counter++;
-            n /= 2;
+
+        // Step1: FInd the num of bits
+        // Step2: Create a mask with all 1s
+        int mask = 0;
+        int temp = n;
+        while(temp > 0){
+            mask = (mask << 1) | 1;
+            temp >>= 1;
         }
-        return res;
+        // Step3: XOR num with mask
+        return n ^ mask;
     }
 }
