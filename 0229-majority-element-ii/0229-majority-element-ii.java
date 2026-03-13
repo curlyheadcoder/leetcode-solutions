09a15b897d3dd42d6513dd28dabe7e8c22c74f7b
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-03-13 19:31:28 +0530

    Time: 2 ms (99.8%), Space: 52.2 MB (79.54%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int maj1 = 0, maj2 = 0;
-        int count1 = 0, count2  = 0;
+        int c1 = 0, c2 = 0;
         int n = nums.length;
         for(int i = 0; i < n; i++){
             if(nums[i] == maj1){
-                count1++;
+                c1++;
             }else if(nums[i] == maj2){
-                count2++;
-            }else if(count1 == 0){
+                c2++;
+            }else if(c1 == 0){
                 maj1 = nums[i];
-                count1 = 1;
-            }else if(count2 == 0){
+                c1 = 1;
+            }else if(c2 == 0){
                 maj2 = nums[i];
-                count2 = 1;
+                c2 = 1;
             }else{
-                count1--;
-                count2--;
+                c1--;
+                c2--;
             }
         }
         List<Integer> ans = new ArrayList<>();
