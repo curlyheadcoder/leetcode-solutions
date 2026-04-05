1829cf5c5eba5a648adf6ae09930a78803d5a54a
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-04-05 14:04:42 +0530

    Time: 2 ms (100%), Space: 46.1 MB (34.7%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int x = 0, y = 0;
-        for(char move : moves.toCharArray()){
-            if(move == 'U') y--;
-            else if(move == 'D') y++;
-            else if(move == 'L') x--;
-            else if(move == 'R') x++;
+        int[] ch = new int[26];
+        for (char move : moves.toCharArray()){
+            ch[move - 'A']++;
         }
-        return x == 0 && y == 0;
+        return ch['U' - 'A'] == ch['D' - 'A'] && 
+                ch['L' - 'A'] == ch['R' - 'A'];
     }
 }
