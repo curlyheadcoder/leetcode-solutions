d61648b3b2bfc216634b97955a7f5ee5b3255725
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-04-14 21:24:34 +0530

    Time: 2 ms (91.2%), Space: 47.7 MB (63.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -6,7 +6,8 @@ class Solution {
                 nums[slow] = nums[fast];
                 slow++;
             }
-        }
+        }    // 1 3 12 3 12     THis is done till now
+        // we have to update last two 3 , 12 to zeroes 
         while(slow < nums.length){
             nums[slow] = 0;
             slow++;
