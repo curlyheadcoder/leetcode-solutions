e81512f601e2d7270c333e5e1fedd5712a7a85e0
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-04-24 22:25:27 +0530

    Time: 29 ms (92.94%), Space: 58.9 MB (90.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        // we want to use Two Pointers 
+        // we will do sorting first
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        for(int i = 0; i < n - 2; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int left = i + 1, right = n - 1;
-            while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0){
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-                    while(left < right && nums[left] == nums[left-1]) left++;
-                    while(left < right && nums[right] == nums[right+1]) right--;
-                }else if(sum < 0) left++;
-                else right--;
+        List<List<Integer>> triplets = new ArrayList<>();
+        for(int i = 0; i < nums.length && nums[i] <= 0; i++){
+            if(i == 0 || nums[i-1] != nums[i]){
+                // Two Sum II
+                twoSum(nums, i, triplets);
             }
         }
-        return res;
+        return triplets;
+    }
+    private void twoSum(int [] nums, int i, List<List<Integer>> triplet){
+        int left = i+1, right = nums.length-1;
+        while(left < right){
+            int sum = nums[i] + nums[left] + nums[right];
+            if(sum == 0){
+                triplet.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                while(left < right && nums[left] == nums[left-1]){
+                    left++;        // duplicate found (skip)
+                }
+            }else if(sum < 0){
+                left++;
+            }else{
+                right--;
+            }
+        }
     }
 }
