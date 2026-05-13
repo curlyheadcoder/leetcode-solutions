0e1b49b062b1c4539ec1f3ac20dcd5d03cc8bbf6
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-05-13 21:33:41 +0530

    Time: 1 ms (86.88%), Space: 47.1 MB (86.17%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,22 +1,29 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        boolean [] visited = new boolean [n];
+    public int findCircleNum(int[][] graph) {
+        // visited boolean array
+        int n = graph.length;      // no of nodes (vertices)
+        boolean [] visited = new boolean[n];
         int provinces = 0;
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                dfs(isConnected, visited, i);
+
+        // since given graph is undirected and acyclic so we will use DFS/BFS
+        // it means it is not connected
+
+        for(int city = 0; city < n; city++){
+            if(!visited[city]){
+                dfs(graph, visited, city);
                 provinces++;
             }
         }
+
         return provinces;
     }
-    private void dfs(int [][] isConnected, boolean [] visited, int city){
-       visited[city] = true;
-       for(int neighbor = 0; neighbor < isConnected.length; neighbor++){
-            if(isConnected[city][neighbor] == 1 && !visited[neighbor]){
-                dfs(isConnected, visited, neighbor);
+
+    private void dfs(int [][] graph, boolean [] visited, int city){
+        visited[city] = true;
+        for(int nn = 0; nn < graph.length; nn++){       // nn  ---> neighbor city
+            if(graph[city][nn] == 1 && !visited[nn]){
+                dfs(graph, visited, nn);
             }
-       }
+        }
     }
 }
