0a9ace192585ff6fab3fb449a1104160319c879d
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-08-02 21:54:25 +0530

    Time: 1 ms (77.22%), Space: 46.8 MB (24.98%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,7 +3,8 @@ class Solution {
         int slow = 0;
         for(int fast = 0; fast < nums.length; fast++){
             if(nums[slow] != nums[fast]){
-                nums[++slow] = nums[fast];
+                ++slow;
+                nums[slow] = nums[fast];
             }
         }
         return slow+1;
