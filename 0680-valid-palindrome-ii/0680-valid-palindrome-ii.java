09b64f5a73d8047b9dabe7b39b2d807aed018ea8
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-08-04 18:39:11 +0530

    Time: 4 ms (99.02%), Space: 47.6 MB (86.3%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
+        int left = 0, right = s.length()-1;
         while(left < right){
             if(s.charAt(left) == s.charAt(right)){
                 left++;
