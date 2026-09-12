0a3f4c921180846658540807422ba09aa97535ea
Mayank Sonwani <mayanksonwani078@gmail.com>
2026-09-12 18:12:15 +0530

    Time: 2 ms (99.95%), Space: 52.7 MB (41.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,33 +1,37 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int maj1 = 0, maj2 = 0;
-        int c1 = 0, c2 = 0;
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == maj1){
-                c1++;
-            }else if(nums[i] == maj2){
-                c2++;
-            }else if(c1 == 0){
-                maj1 = nums[i];
-                c1++;
-            }else if(c2 == 0){
-                maj2 = nums[i];
-                c2++;
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == candidate1){
+                count1++;
+            }else if(nums[i] == candidate2){
+                count2++;
+            }else if(count1 == 0){
+                candidate1 = nums[i];
+                count1++;
+            }else if(count2 == 0){
+                candidate2 = nums[i];
+                count2++;
             }else{
-                c1--;
-                c2--;
+                count1--;
+                count2--;
             }
         }
+        // verify 
+        count1 = 0;
+        count2 = 0;
         List<Integer> ans = new ArrayList<>();
-        int freq1 = 0, freq2 = 0;
         for(int num : nums){
-            if(num == maj1) freq1++;
-            else if(num == maj2) freq2++;
+            if(num == candidate1) count1++;
+            else if(num == candidate2) count2++;
         }
-        if(freq1 > n/3) ans.add(maj1);
-        if(freq2 > n/3) ans.add(maj2);
-
+        if(count1 > nums.length/3){
+            ans.add(candidate1);
+        }
+        if(count2 > nums.length/3){
+            ans.add(candidate2);
+        }
         return ans;
     }
 }
